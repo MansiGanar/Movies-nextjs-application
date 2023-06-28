bfdb73b708b06dbbb9af9217beda4a8509b2cd3a
Mansi Ganar <mansi99ganar@gmail.com>
2023-06-28 20:23:44 +0200

    img in pagge.tsx is replaced with Image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: "center" }}>
       <Header>Home</Header>
-      <img
+      <Image
         style={{ width: "70%", height: "90vh", margin: "auto" }}
         src="https://cdn.dribbble.com/users/1575908/screenshots/7481309/media/8c9c4b4ca4421234f98643199054813c.jpg?compress=1&resize=1000x750&vertical=center"
         alt="movie-img"
